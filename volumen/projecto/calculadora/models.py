f91b1e5bd60753a3c51cbb53a50ac1f110622a9c
Matias Lionel Ceballos <m.lionel.tkd@gmail.com>
2019-03-26 00:07:53 -0300

    Calculadora basica implementada

--- volumen/projecto/calculadora/models.py
+++ volumen/projecto/calculadora/models.py
@@ -1,22 +1,10 @@
 from django.db import models
+from django.forms import ModelForm
 
+class Input(models.Model):
+    r = models.FloatField()
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-    def __str__(self):
-        return self.question_text
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
-
-# Create your models here.
+class InputForm(ModelForm):
+    class Meta:
+        model = Input
+        fields = "__all__"
--- volumen/projecto/calculadora/views.py
+++ volumen/projecto/calculadora/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext
 from django.http import HttpResponse
-
+from .models import InputForm
+from .calcular import compute
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            return present_output(form)
+    else:
+        form = InputForm()
+    context_instance = RequestContext(request)
+    return render(request, 'calculadora1.html', {'form': form})
+    #return render_to_response('calculadora1.html', {'form': form}, context_instance)
+
+def present_output(form):
+    r = form.r
+    s = compute(r)
+    return HttpResponse('Hello, World! sin(%s)=%s' % (r, s))
